d60ec3ad77af556446211fe403797d69ba9ee513
taoqf <tao_qiufeng@126.com>
2022-09-24 16:05:45 +0800

    env

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import { createCipheriv, createDecipheriv, createHash } from 'crypto';
-import mmconf from '@mmstudio/config';
 
-const config = mmconf as {
-	file: {
-		secret: string;
-	}
-};
+const iv = process.env.FILE_SECRET!;
 
-const iv = config.file.secret;
 const key = createHash('md5').update(iv).digest('hex');	// 长度32
 const algorithm = 'aes-256-gcm';	// 'aes-128-gcm' | 'aes-192-gcm' | 'aes-256-gcm'
 const encrypter = createCipheriv(algorithm, key, iv);
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,17 @@
 const test = require('ava');
+const { loadEnvConfig } = require('@next/env');
+loadEnvConfig('./');
+const { default: an61 } = require('../dist/index');
 
-const { default: a } = require('../dist/index');
+const vo = '01factory';
+const vt = Buffer.from('XH/jv/86ds0T', 'base64');
 
-test('xxx', (t) => {
-	t.pass();
+test('encrypt', (t) => {
+	const r = an61.encrypt(Buffer.from(vo, 'utf-8'));
+	t.deepEqual(r, vt);
+});
+
+test('dencrypt', (t) => {
+	const r = an61.decrypt(vt);
+	t.is(r.toString('utf-8'), vo);
 });
