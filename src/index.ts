1f72707c09ee2413a86cf9e81adbc4c9014e9c25
taoqf <tao_qiufeng@126.com>
2022-09-24 09:23:12 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,33 @@
+import { createCipheriv, createDecipheriv, createHash } from 'crypto';
+import mmconf from '@mmstudio/config';
 
-export default function todo() {
-	// todo
+const config = mmconf as {
+	file: {
+		secret: string;
+	}
+};
+
+const iv = config.file.secret;
+const key = createHash('md5').update(iv).digest('hex');	// 长度32
+const algorithm = 'aes-256-gcm';	// 'aes-128-gcm' | 'aes-192-gcm' | 'aes-256-gcm'
+const encrypter = createCipheriv(algorithm, key, iv);
+const decrypter = createDecipheriv(algorithm, key, iv);
+
+/**
+ * 加密文件
+ */
+export function encrypt(buf: Buffer) {
+	return encrypter.update(buf);
 }
+
+/**
+ * 解密文件
+ */
+export function decrypt(buf: Buffer) {
+	return decrypter.update(buf);
+}
+
+export default {
+	encrypt,
+	decrypt
+};
